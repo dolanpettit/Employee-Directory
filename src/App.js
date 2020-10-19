8c3427bd8f0c129b95f61ac1b8236f7334adba3b
Dolan Pettit <dolanpettit@gmail.com>
2020-10-19 16:28:39 -0400

    Update: Created component mount logic for random users

--- src/App.js
+++ src/App.js
@@ -5,3 +5,20 @@ import Search from "./components/Search";
 import Table from "./components/Table";
 import API from "./utils/API";
 import Footer from "./components/Footer";
+
+class App extends Component {
+  state = {
+    search: "",
+    users: [],
+  };
+
+  componentDidMount() {
+    API.getRandomUsers().then((res) =>
+      this.setState({ users: res.data.results }).catch((err) =>
+        console.log(err)
+      )
+    );
+  }
+
+  
+}
