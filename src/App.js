ddca740fdf8fac03435fba115cea8eb84e2fd3c9
Dolan Pettit <dolanpettit@gmail.com>
2020-10-19 16:25:06 -0400

    Update: Import to app.js

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import "./App.css";
+import Jumbotron from "./components/Jumbotron";
+import Search from "./components/Search";
+import Table from "./components/Table";
+import API from "./utils/API";
+import Footer from "./components/Footer";
